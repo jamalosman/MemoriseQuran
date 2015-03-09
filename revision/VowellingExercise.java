e1e84d663d1908bed0b8dcc3f1fcd2e3df36fbfc
jamal <jamal.osman93@gmail.com>
2015-03-09 13:39:08 +0000

    added res folder and rest of java folder

--- revision/VowellingExercise.java
+++ revision/VowellingExercise.java
@@ -1,15 +1,26 @@
 package com.gre.jamal.memorisequran.revision;
 
+import org.jqurantree.arabic.ArabicCharacter;
 import org.jqurantree.orthography.Verse;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by jamal on 09/03/15.
  */
 public class VowellingExercise extends MemoryExercise {
 
+    private ArrayList<ArabicCharacter> shortVowels;
 
     protected VowellingExercise(Verse verse) {
         super(verse);
+        shortVowels = new ArrayList<>();
+        String[] verseWords = verse.toUnicode().split(" ");
+        Iterator<ArabicCharacter> letterItr = verse.iterator();
+        while (letterItr.hasNext()){
+
+        }
     }
 
     @Override
